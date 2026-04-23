41dc8c6ded2f03fd760d79e1a1f3768e02505163
Matiss Priede <matiss.priede07@gmail.com>
2026-04-23 10:05:44 +0000

    add navbar and database connection, login page

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import sqlite3
+import os
 
 app = Flask(__name__)
+DB = os.path.join(os.path.dirname(__file__), 'database.db')
+
+
+def get_db():
+    conn = sqlite3.connect(DB)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    db = get_db()
+    continents = db.execute('SELECT * FROM continents').fetchall()
+    db.close()
+    return render_template('index.html', continents=continents)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
