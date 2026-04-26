fc92e596c6496310388ae728d9619eb7bb34515c
Matiss Priede <matiss.priede07@gmail.com>
2026-04-26 11:32:55 +0000

    add continent page and fix nav links

--- app.py
+++ app.py
@@ -20,6 +20,34 @@ def index():
     return render_template('index.html', continents=continents)
 
 
+@app.route('/continent/<int:continent_id>')
+def continent(continent_id):
+    db = get_db()
+    cont = db.execute('SELECT * FROM continents WHERE id = ?', (continent_id,)).fetchone()
+    if not cont:
+        return "Not found", 404
+
+    destinations = db.execute(
+        'SELECT * FROM destinations WHERE continent_id = ?', (continent_id,)
+    ).fetchall()
+
+    dest_data = []
+    for dest in destinations:
+        tours = db.execute(
+            'SELECT * FROM tours WHERE destination_id = ?', (dest['id'],)
+        ).fetchall()
+        tours_with_stops = []
+        for tour in tours:
+            stops = db.execute(
+                'SELECT stop_name FROM tour_stops WHERE tour_id = ?', (tour['id'],)
+            ).fetchall()
+            tours_with_stops.append({'tour': tour, 'stops': stops})
+        dest_data.append({'dest': dest, 'tours': tours_with_stops})
+
+    db.close()
+    return render_template('continent.html', continent=cont, dest_data=dest_data)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
